4bcb8d2a32fd34cdc59268f47852265ac751e898
mrnathanhunt <nathan@gatewaytouae.com>
2025-08-26 17:38:02 -0500

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Thrust Portal running on http://localhost:${port}`);
+});
 
